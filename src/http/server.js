34f9a5a4307b59cfe47ef9d13f82ce084f18fa69
tiago-egas <tiagoegas@outlook.com>
2024-11-05 00:26:30 -0300

    Route Create Goal
    Signed-off-by: tiago-egas <tiagoegas@outlook.com>

--- src/http/server.js
+++ src/http/server.js
@@ -4,9 +4,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
-const app = (0, fastify_1.default)();
-app.listen({
+const create_goal_1 = require("../features/create-goal");
+const fastify_type_provider_zod_1 = require("fastify-type-provider-zod");
+const zod_1 = __importDefault(require("zod"));
+const app = (0, fastify_1.default)().withTypeProvider();
+app.setValidatorCompiler(fastify_type_provider_zod_1.validatorCompiler);
+app.setSerializerCompiler(fastify_type_provider_zod_1.serializerCompiler);
+app.post("/goals", {
+    schema: {
+        body: zod_1.default.object({
+            title: zod_1.default.string(),
+            desiredWeeklyFrequency: zod_1.default.number().int().min(1).max(7),
+        }),
+    },
+}, async (request) => {
+    const { title, desiredWeeklyFrequency } = request.body;
+    await (0, create_goal_1.createGoal)({
+        title,
+        desiredWeeklyFrequency,
+    });
+});
+app
+    .listen({
     port: 3333,
-}).then(() => {
-    console.log('Server is running on port 3333');
+})
+    .then(() => {
+    console.log("Server is running on port 3333");
 });
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,41 @@
-import fastify from 'fastify';
+import fastify from "fastify";
+import {createGoal} from "../features/create-goal";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log('Server is running on port 3333');
-})
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+	"/goals",
+	{
+		schema: {
+			body: z.object({
+				title: z.string(),
+				desiredWeeklyFrequency: z.number().int().min(1).max(7),
+			}),
+		},
+	},
+	async (request) => {
+		const { title, desiredWeeklyFrequency } = request.body;
+
+		await createGoal({
+			title,
+			desiredWeeklyFrequency,
+		});
+	},
+);
+
+app
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log("Server is running on port 3333");
+	});
